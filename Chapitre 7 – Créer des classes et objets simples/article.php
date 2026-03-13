1107067e5884eacda02e109686f6ad8d1918a69b
OUSSAMA <oussamacharef71@gmail.com>
2026-03-13 17:02:25 +0000

    add chapitere 7

--- Chapitre 7 – Créer des classes et objets simples/article.php
+++ Chapitre 7 – Créer des classes et objets simples/article.php
@@ -32,3 +32,24 @@ echo "<br>" . $article2->afficher();
 
 
 ?>
+
+
+
+
+<?php
+
+// class Article {
+//     public $titre;
+//     public $contenu;
+
+//     public function afficher() {
+//         return "Titre : " . $this->titre . " - Contenu : " . $this->contenu;
+//     }
+// }
+
+// $article1 = new Article("Introduction à PHP", "PHP est un langage de script côté serveur.");
+// echo $article1->afficher()."<br>";
+// $article2 = new Article("Programmation orientée objet", "La POO facilite la modularité et la maintenance.");
+// echo $article2->afficher()."<br>";
+
+?>
